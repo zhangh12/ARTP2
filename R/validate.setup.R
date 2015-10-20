dfe567aac04840c237c4fee4b102bdaa43e1084c
Han Zhang <zhangh.ustc@gmail.com>
2015-10-20 15:35:38 -0400

    ARTP3 v0.8.8
    pathway.warm.start supports path as input again, but we are not going
    to mention this in help doc

--- R/validate.setup.R
+++ R/validate.setup.R
@@ -1,6 +1,19 @@
 
 validate.setup <- function(setup){
   
+  if(is.character(setup)){
+    ld <- try(load(setup), silent = TRUE)
+    if(error.try(ld)){
+      msg <- paste('Cannot load', setup)
+      stop(msg)
+    }
+    
+    if(!is.list(setup)){
+      msg <- paste('Invalid components in', setup)
+      stop(msg)
+    }
+  }
+  
   setup$options$only.setup <- NULL
   setup$options$save.setup <- NULL
   setup$options$path.setup <- NULL
