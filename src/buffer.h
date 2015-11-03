21a48066a965052c09708edb96b36ef6bfd46230
Yifan <yfyang.86@gmail.com>
2015-11-03 13:22:13 -0500

    initial buffer.h:

---
+++ src/buffer.h
@@ -0,0 +1,17 @@
+#ifndef BUFFER_BIN_ARTPS
+#define BUFFER_BIN_ARTPS
+/*
+Task:
+Buffer reading binary file,
+given file size (nrow ncol)
+all data are float
+
+.815
+artp3.cpp:
+- artp3_chr    L672-683
+- adajoint_chr L924-936
+- artp3        L1059-1069
+
+*/
+
+#endif
